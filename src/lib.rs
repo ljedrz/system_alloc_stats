2ed39f36380cf8c6d2c4bfc2e1fb6fb314e84872
ljedrz <ljedrz@gmail.com>
2023-08-10 14:38:50 +0200

    bulk: rename the statics, add a SystemStats object
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,30 @@ use std::{
 };
 
 #[derive(Debug, Default, Clone, Copy)]
-pub struct AllocStats;
+pub struct SystemWithStats;
+
+#[derive(Debug, Default, Clone)]
+pub struct SystemStats {
+    pub alloc_count: usize,
+    pub alloc_avg: Option<usize>,
+    pub realloc_growth_count: usize,
+    pub realloc_growth_avg: Option<usize>,
+    pub realloc_shrink_count: usize,
+    pub realloc_shrink_avg: Option<usize>,
+    pub use_curr: usize,
+    pub use_max: usize,
+}
 
 static ALLOC_COUNT: AtomicUsize = AtomicUsize::new(0);
 static ALLOC_SUM: AtomicUsize = AtomicUsize::new(0);
-static CURR_HEAP: AtomicUsize = AtomicUsize::new(0);
-static MAX_HEAP: AtomicUsize = AtomicUsize::new(0);
 static REALLOC_GROWTH_COUNT: AtomicUsize = AtomicUsize::new(0);
 static REALLOC_GROWTH_SUM: AtomicUsize = AtomicUsize::new(0);
 static REALLOC_SHRINK_COUNT: AtomicUsize = AtomicUsize::new(0);
 static REALLOC_SHRINK_SUM: AtomicUsize = AtomicUsize::new(0);
+static USE_CURR: AtomicUsize = AtomicUsize::new(0);
+static USE_MAX: AtomicUsize = AtomicUsize::new(0);
 
-impl AllocStats {
+impl SystemWithStats {
     pub fn alloc_count(&self) -> usize {
         ALLOC_COUNT.load(Ordering::Relaxed)
     }
@@ -40,59 +52,87 @@ impl AllocStats {
         REALLOC_SHRINK_SUM.load(Ordering::Relaxed)
     }
 
-    pub fn avg_alloc_size(&self) -> usize {
+    pub fn avg_alloc_size(&self) -> Option<usize> {
         let sum = ALLOC_SUM.load(Ordering::Relaxed);
         let count = ALLOC_COUNT.load(Ordering::Relaxed);
-        sum / count
+        sum.checked_div(count)
     }
 
-    pub fn avg_realloc_growth_size(&self) -> usize {
+    pub fn avg_realloc_growth_size(&self) -> Option<usize> {
         let sum = REALLOC_GROWTH_SUM.load(Ordering::Relaxed);
         let count = REALLOC_GROWTH_COUNT.load(Ordering::Relaxed);
-        sum / count
+        sum.checked_div(count)
     }
 
-    pub fn avg_realloc_shrink_size(&self) -> usize {
+    pub fn avg_realloc_shrink_size(&self) -> Option<usize> {
         let sum = REALLOC_SHRINK_SUM.load(Ordering::Relaxed);
         let count = REALLOC_SHRINK_COUNT.load(Ordering::Relaxed);
-        sum / count
+        sum.checked_div(count)
     }
 
-    pub fn current_use(&self) -> usize {
-        CURR_HEAP.load(Ordering::Relaxed)
+    pub fn use_curr(&self) -> usize {
+        USE_CURR.load(Ordering::Relaxed)
     }
 
-    pub fn max_use(&self) -> usize {
-        MAX_HEAP.load(Ordering::Relaxed)
+    pub fn use_max(&self) -> usize {
+        USE_MAX.load(Ordering::Relaxed)
     }
 
     pub fn reset(&self) {
         ALLOC_SUM.store(0, Ordering::Relaxed);
         ALLOC_COUNT.store(0, Ordering::Relaxed);
-        MAX_HEAP.store(self.current_use(), Ordering::Relaxed);
         REALLOC_GROWTH_COUNT.store(0, Ordering::Relaxed);
         REALLOC_GROWTH_SUM.store(0, Ordering::Relaxed);
         REALLOC_SHRINK_COUNT.store(0, Ordering::Relaxed);
         REALLOC_SHRINK_SUM.store(0, Ordering::Relaxed);
+        USE_MAX.store(self.use_curr(), Ordering::Relaxed);
+    }
+
+    pub fn stats(&self) -> SystemStats {
+        let alloc_count = self.alloc_count();
+        let alloc_sum = self.alloc_sum();
+        let alloc_avg = alloc_sum.checked_div(alloc_count);
+
+        let realloc_growth_count = self.realloc_growth_count();
+        let realloc_growth_sum = self.realloc_growth_sum();
+        let realloc_growth_avg = realloc_growth_sum.checked_div(realloc_growth_count);
+
+        let realloc_shrink_count = self.realloc_shrink_count();
+        let realloc_shrink_sum = self.realloc_shrink_sum();
+        let realloc_shrink_avg = realloc_shrink_sum.checked_div(realloc_shrink_count);
+
+        let use_curr = self.use_curr();
+        let use_max = self.use_max();
+
+        SystemStats {
+            alloc_count,
+            alloc_avg,
+            realloc_growth_count,
+            realloc_growth_avg,
+            realloc_shrink_count,
+            realloc_shrink_avg,
+            use_curr,
+            use_max,
+        }
     }
 }
 
-unsafe impl GlobalAlloc for AllocStats {
+unsafe impl GlobalAlloc for SystemWithStats {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let ret = System.alloc(layout);
         if !ret.is_null() {
             let size = layout.size();
             ALLOC_SUM.fetch_add(size, Ordering::Relaxed);
             ALLOC_COUNT.fetch_add(1, Ordering::Relaxed);
-            let curr = CURR_HEAP.fetch_add(size, Ordering::Relaxed);
-            MAX_HEAP.fetch_max(curr, Ordering::Relaxed);
+            let curr = USE_CURR.fetch_add(size, Ordering::Relaxed);
+            USE_MAX.fetch_max(curr, Ordering::Relaxed);
         }
         ret
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         System.dealloc(ptr, layout);
-        CURR_HEAP.fetch_sub(layout.size(), Ordering::Relaxed);
+        USE_CURR.fetch_sub(layout.size(), Ordering::Relaxed);
     }
 
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
